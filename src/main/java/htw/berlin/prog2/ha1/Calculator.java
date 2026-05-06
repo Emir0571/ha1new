329abbf764e6fb86fcf87ab27d8b4ac24b3f86db
kavurur1907 <Emir.Kavurur@Student.HTW-Berlin.de>
2026-05-06 13:29:09 +0200

    Bugfix 2. Red test (failed test)

--- src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -44,10 +44,19 @@ public class Calculator {
      * Werte sowie der aktuelle Operationsmodus zurückgesetzt, so dass der Rechner wieder
      * im Ursprungszustand ist.
      */
+
+    private boolean clearPressedOnce = false;
+
     public void pressClearKey() {
-        screen = "0";
-        latestOperation = "";
-        latestValue = 0.0;
+        if(!clearPressedOnce){
+            screen = "0";
+            clearPressedOnce = true;
+        } else {
+            screen = "0";
+            latestOperation = "";
+            latestValue = 0.0;
+            clearPressedOnce = false;
+        }
     }
 
     /**
